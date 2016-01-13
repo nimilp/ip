b566ecd41a27780fa99b3174c5a2b16f47e26eb6
Nimil Peethambaran <nimil.p@hotmail.com>
2016-01-12 21:18:17 -0600

    first change

---
+++ src/main/java/com/npeetha/ip/servlets/IpServlet.java
@@ -0,0 +1,26 @@
+package com.npeetha.ip.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IpServlet extends HttpServlet{
+
+	private static String ipAddress;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+		resp.getOutputStream().print("<html><head/><body>"+ipAddress+"</body></html>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+		ipAddress = req.getRemoteAddr();
+	}
+}

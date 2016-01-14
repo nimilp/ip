546e32b6c60d85a11ef70af645ee4abf8802ae14
Nimil Peethambaran <nimil.p@hotmail.com>
2016-01-13 21:11:04 -0600

    new rest resource

--- src/main/java/com/npeetha/ip/resources/TaskResource.java
+++ src/main/java/com/npeetha/ip/resources/TaskResource.java
@@ -1,13 +1,17 @@
 package com.npeetha.ip.resources;
 
+import javax.print.attribute.standard.Media;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/tasks")
 public class TaskResource {
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getTasks(){
 		return  Response.ok("me").build();
 	}
